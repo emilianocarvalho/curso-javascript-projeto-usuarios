6ee6743017ce4371562324f3eaf3caf4b8c72e44
Emiliano Carvalho <emilianocarvalho.dev@gmail.com>
2018-10-10 02:12:22 -0300

    Serializando o formulário

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-let name = document.querySelector('#exampleInputName');
-let gender = document.querySelectorAll(
-  '#form-user-create [name=gender]:checked'
-);
-let birth = document.querySelector('#exampleInputBirth');
-let country = document.querySelector('#exampleInputCountry');
-let email = document.querySelector('#exampleInputEmail');
-let password = document.querySelector('#exampleInputPassword');
-let photo = document.querySelector('#exampleInputPhoto');
-let admin = document.querySelector('#exampleInputAdmin');
-
-// Selectionando o option correto
-
-console.log(gender.value);
-
 var fields = document.querySelectorAll('#form-user-create [name]');
 
+var user = {};
+
 fields.forEach(function(field, index) {
+
   if (field.name === 'gender') {
-    if (field.checked) console.log('SIM', field);    
+
+    if (field.checked) {
+      user[field.name] = field.value;
+    }
+
   } else {
-    console.log('NÃO', field);
+
+    user[field.name] = field.value;
+
   }
-  
+
 });
+
+console.log(user);
+
